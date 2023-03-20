9ac2b07f01f88e7fc0801c2e10ae27675a7301e8
khpark0203 <khpark0203@gmail.com>
2023-03-20 21:24:04 +0900

    status code test

--- router/router_v1.go
+++ router/router_v1.go
@@ -1,7 +1,28 @@
 package router
 
-import "goarch/handler"
+import (
+	"goarch/handler"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (a *api) InitV1() {
 	a.v1.GET("/users", handler.GetUserList)
+
+	a.v1.GET("/status/:statuscode", func(c *gin.Context) {
+		code, err := strconv.Atoi(c.Param("statuscode"))
+		if err != nil {
+			c.AbortWithError(500, err)
+			return
+		}
+
+		if code < 100 || code > 600 {
+			code = 200
+		}
+
+		c.JSON(code, gin.H{
+			"result": "success",
+		})
+	})
 }
